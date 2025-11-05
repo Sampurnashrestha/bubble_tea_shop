eb861c90d308a36152a9376d4f1ef33b5f39b700
Sampurnashrestha <shresthasampurna849@gmail.com>
2025-11-05 06:56:27 +0545

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Pages.css'
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home-container">
+      <div className="hero">
+        <h1>We  lcome</h1>
+        <h2>to Leafora</h2>
+        <p>Freshly brewed happiness in every cup 🍹</p>
+
+        <div className="product-gallery">
+          <div className="porduct-card">
+            <img src="../image/bubble-tea1.jpg" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
