ed9544138aa9088931658214a7ec6cbdf22c6db3
Sampurnashrestha <shresthasampurna849@gmail.com>
2025-11-06 06:56:43 +0545

    manage the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import './Pages.css'
+import React from "react";
+import "./Pages.css";
+import BubbleTea1 from "../image/bubble-tea1.png";
+import BubbleTea2 from "../image/bubble-tea2.jpg";
+import BubbleTea3 from "../image/bubble-tea3.jpg";
 
 function Home() {
   return (
     <div className="home-container">
       <div className="hero">
-        <h1>We  lcome</h1>
-        <h2>to Leafora</h2>
-        <p>Freshly brewed happiness in every cup 🍹</p>
+       
 
         <div className="product-gallery">
-          <div className="porduct-card">
-            <img src="../image/bubble-tea1.jpg" alt="" />
-            <img src="../image/bubble-tea1.jpg" alt="" />
-            <img src="../image/bubble-tea1.jpg" alt="" />
+          <div className="product-card">
+               <h3>Bubble Tea</h3>
+            <span>price: rs 200 (10% discount)</span>
+            <img src={BubbleTea1} alt="" />
           </div>
+          {/* <div className="product-card">
+            <img src={BubbleTea2} alt="" />
+             <h3>Bubble Tea</h3>
+            <span>price: rs 200 (10% discount)</span>
+          </div>
+          <div className="product-card">
+            <img src={BubbleTea3} alt="" />
+             <h3>Bubble Tea</h3>
+            <span>price: rs 200 (10% discount)</span> */}
+          {/* </div> */}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
